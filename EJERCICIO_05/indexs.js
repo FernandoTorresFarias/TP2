9126c43e4dccea6e6a563948441f1e20cdc77b96
FernandoTorresFarias <FernandoFariasTorres@outlook.es>
2024-05-30 11:50:38 -0300

    Se sube nuevamente ejercicio 05 corregiendo errores

--- EJERCICIO_05/indexs.js
+++ EJERCICIO_05/indexs.js
@@ -3,14 +3,15 @@ function calculos(){
     let B = document.getElementById("B").value
     let C = document.getElementById("C").value
 
-    if(A => 0 && B > 0 && C > 0 && A >= C ){
-    alert("Error,debe ingresar numeros positivos y/o corregir sus lados")
-    return
+    if (A > 0 && B > 0 && C > 0 && A > C) {
+        let triangulo = B * (A-C) / 2;
+        let rectangulo =  A * B
+        let resultado = triangulo+rectangulo
+        alert(`Su Area es: ${resultado}`)
     }
- let triangulo = B * (A-C) / 2;
- let rectangulo =  A * B
- let resultado = triangulo+rectangulo
- alert(`Su Area es: ${resultado}`)
+    else{
+        alert("Error,debe ingresar numeros positivos y/o corregir sus lados")
+    }
 
     
 }
