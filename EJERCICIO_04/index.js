e7c2c5e1624ac8603b9b6b853f95b5596a31148e
FernandoTorresFarias <FernandoFariasTorres@outlook.es>
2024-05-30 11:23:37 -0300

    Se sube nuevamente ejercicio cuatro,corregiendo errores

--- EJERCICIO_04/index.js
+++ EJERCICIO_04/index.js
@@ -3,7 +3,7 @@ function calculos(){
     let altura = document.getElementById("altura").value
     
     if ( peso > 0 && altura > 0) {
-        let resultado = peso / altura
+        let resultado = peso / altura *2
         alert(`Su IMC: ${resultado}`)
     }
     else{
