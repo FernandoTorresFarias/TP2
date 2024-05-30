5b865b7b0e01c6db9347d7f068826be220ee17a1
FernandoTorresFarias <FernandoFariasTorres@outlook.es>
2024-05-30 01:57:14 -0300

    Se sube ejercicio 05

---
+++ EJERCICIO_05/indexs.js
@@ -0,0 +1,17 @@
+function calculos(){
+    let A = document.getElementById("A").value
+    let B = document.getElementById("B").value
+    let C = document.getElementById("C").value
+
+    if(A => 0 && B > 0 && C > 0 && A >= C ){
+    alert("Error,debe ingresar numeros positivos y/o corregir sus lados")
+    return
+    }
+ let triangulo = B * (A-C) / 2;
+ let rectangulo =  A * B
+ let resultado = triangulo+rectangulo
+ alert(`Su Area es: ${resultado}`)
+
+    
+}
+
